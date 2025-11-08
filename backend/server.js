cc388d88a64297fe4931d2155a974894c18bf4d0
Poojan P Shrivastav <poojanshrivastav21@gmail.com>
2025-11-09 00:26:24 +0530

    project Completed.

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+.then(() => console.info('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -25,4 +25,6 @@ app.use('/api/blogs', require('./routes/blogs'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.info(`Server running on port ${PORT}`);
+});
